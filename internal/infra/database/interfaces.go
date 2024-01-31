1866261d0794ba52e27a090244749abe2dcecdff
GusGD <si_gustavo@Outlook.com>
2024-01-31 02:45:15 -0300

    Create ProductDBInterface

--- internal/infra/database/interfaces.go
+++ internal/infra/database/interfaces.go
@@ -6,3 +6,11 @@ type UserDBInterface interface {
 	Create(user *entity.User) error
 	FindByEmail(email string) (*entity.User, error)
 }
+
+type ProductDBInterface interface {
+	Create(product *entity.Product) error
+	FindAllProduct(page, limit int, sort string) ([]entity.Product, error)
+	FindByProductName(productName string) (*entity.Product, error)
+	Update(product *entity.Product) error
+	Delete(id string) error
+}
