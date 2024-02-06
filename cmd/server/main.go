bee37d409b9669ca0bd402cee68b10ff8a2360ef
GusGD <si_gustavo@Outlook.com>
2024-02-05 22:34:58 -0300

    Created ProductHandler and its endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,61 @@
 package main
 
-import "github.com/gusgd/apigo/configs"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gusgd/apigo/configs"
+	"github.com/gusgd/apigo/internal/dto"
+	"github.com/gusgd/apigo/internal/entity"
+	"github.com/gusgd/apigo/internal/infra/database"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
 
 func main() {
-	configs, _ := configs.LoadConfig(".")
-	println(configs.DBDriver)
+	_, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+	productDB := database.NewProduct(db)
+	productHandler := NewProductHandler(productDB)
+	http.HandleFunc("/products", productHandler.CreateProduct)
+
+	http.ListenAndServe(":8082", nil)
+}
+
+type ProductHandler struct {
+	ProductDB database.ProductDBInterface
+}
+
+func NewProductHandler(db database.ProductDBInterface) *ProductHandler {
+	return &ProductHandler{
+		ProductDB: db,
+	}
+}
+
+func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var product dto.CreateProductInput
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	p, err := entity.NewProduct(product.Name, product.Price)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	err = h.ProductDB.Create(p)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
 }
