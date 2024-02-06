bf5426bd1c735b5ce9925076d173c50fa88af4f4
GusGD <si_gustavo@Outlook.com>
2024-02-05 22:11:40 -0300

    ProductDBInterface Fix

--- internal/infra/database/interfaces.go
+++ internal/infra/database/interfaces.go
@@ -11,7 +11,7 @@ type ProductDBInterface interface {
 	Create(product *entity.Product) error
 	FindAll(page, limit int, sort string) ([]entity.Product, error)
 	FindByName(productName string) (*entity.Product, error)
-	FindBytId(id string) (*entity.Product, error)
+	FindByID(id string) (*entity.Product, error)
 	Update(product *entity.Product) error
 	Delete(id string) error
 }
