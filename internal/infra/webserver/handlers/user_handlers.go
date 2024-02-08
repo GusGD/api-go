8306975a2519891e2d6667701045cb50920d01ac
GusGD <si_gustavo@Outlook.com>
2024-02-08 15:41:21 -0300

    Created the struct Error; Added Error in Handler; Created Docs users

--- internal/infra/webserver/handlers/user_handlers.go
+++ internal/infra/webserver/handlers/user_handlers.go
@@ -2,17 +2,18 @@ package handlers
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
 	"time"
 
-	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/jwtauth"
 	"github.com/gusgd/apigo/internal/dto"
 	"github.com/gusgd/apigo/internal/entity"
 	"github.com/gusgd/apigo/internal/infra/database"
 )
 
+type Error struct {
+	Message string `json:"message"`
+}
 type UserHandler struct {
 	UserDB database.UserDBInterface
 }
@@ -23,6 +24,17 @@ func NewUserHandler(userDB database.UserDBInterface) *UserHandler {
 	}
 }
 
+// Generate JWT godoc
+// @Summary      Get a user JWT Token
+// @Description  Get a user JWT Token
+// @Tags         users
+// @Accept       json
+// @Produce      json
+// @Param        request   body    dto.GetJWTInput       true  "user credentials"
+// @Success      200  {object}  dto.GetJWTOutput
+// @Failure      404  {object}  Error
+// @Failure      500  {object}  Error
+// @Router       /users/generate_token [post]
 func (h *UserHandler) GetJWT(w http.ResponseWriter, r *http.Request) {
 	jwt := r.Context().Value("jwt").(*jwtauth.JWTAuth)
 	jwtExpiresIn := r.Context().Value("JWTExperesIN").(int)
@@ -34,56 +46,59 @@ func (h *UserHandler) GetJWT(w http.ResponseWriter, r *http.Request) {
 	}
 	u, err := h.UserDB.FindByEmail(user.Email)
 	if err != nil {
-		w.WriteHeader(http.StatusUnauthorized)
+		w.WriteHeader(http.StatusNotFound)
+		error := Error{Message: err.Error()}
+		json.NewEncoder(w).Encode(error)
+		return
 	}
 	if !u.ValidatePassword(user.Password) {
 		w.WriteHeader(http.StatusUnauthorized)
+		error := Error{Message: err.Error()}
+		json.NewEncoder(w).Encode(error)
 		return
 	}
 	_, tokenString, _ := jwt.Encode(map[string]interface{}{
 		"sub": u.ID.String(),
 		"exp": time.Now().Add(time.Second * time.Duration(jwtExpiresIn)).Unix(),
 	})
-	acessToken := struct {
-		AcessToken string `json:"acess_token"`
-	}{
-		AcessToken: tokenString,
-	}
+	acessToken := dto.GetJWTOutput{AcessToken: tokenString}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(acessToken)
 }
 
+// Create user godoc
+// @Summary      Create user
+// @Description  Create user
+// @Tags         users
+// @Accept       json
+// @Produce      json
+// @Param        request   body    dto.CreateUserInput       true  "user request"
+// @Success      201
+// @Failure      500  {object}  Error
+// @Router       /users [post]
 func (h *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
 	var user dto.CreateUserInput
 	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
+		error := Error{Message: err.Error()}
+		json.NewEncoder(w).Encode(error)
 		return
 	}
 	u, err := entity.NewUser(user.Name, user.Email, user.Password)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
+		error := Error{Message: err.Error()}
+		json.NewEncoder(w).Encode(error)
+		return
 	}
 	err = h.UserDB.Create(u)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
-	}
-	w.WriteHeader(http.StatusCreated)
-}
-
-func (h *UserHandler) GetUser(w http.ResponseWriter, r *http.Request) {
-	email := chi.URLParam(r, "email")
-	if email == "" {
-		w.WriteHeader(http.StatusBadRequest)
+		error := Error{Message: err.Error()}
+		json.NewEncoder(w).Encode(error)
 		return
 	}
-	user, err := h.UserDB.FindByEmail(email)
-	if err != nil {
-		w.WriteHeader(http.StatusNotFound)
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	log.Print("User email:", email)
-	json.NewEncoder(w).Encode(user)
+	w.WriteHeader(http.StatusCreated)
 }
